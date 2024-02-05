13193c44915391748837fe531dbd301571af934e
sepidekoohfar <sepideh.koohfar@unh.edu>
2024-02-05 09:06:17 -0500

    fixing cuda and cpu

--- textgeneration.py
+++ textgeneration.py
@@ -36,7 +36,7 @@ def collate_fn(batch):
 
     # Pad sequences using tokenizer directly
     encoded_data = tokenizer(
-      sequences, padding=True, truncation=True, max_length=64
+      sequences, padding=True, truncation=True, max_length=512
     )
 
     # Access padded input_ids and labels
@@ -54,9 +54,9 @@ def collate_fn(batch):
 
 loss_fn = torch.nn.CrossEntropyLoss()
 
-train_dataloader = DataLoader(product_reviews_train, batch_size=4, collate_fn=collate_fn)
-valid_dataloader = DataLoader(product_reviews_valid, batch_size=4, collate_fn=collate_fn)
-test_dataloader = DataLoader(product_reviews_test, batch_size=4, collate_fn=collate_fn)
+train_dataloader = DataLoader(product_reviews_train, batch_size=32, collate_fn=collate_fn)
+valid_dataloader = DataLoader(product_reviews_valid, batch_size=32, collate_fn=collate_fn)
+test_dataloader = DataLoader(product_reviews_test, batch_size=32, collate_fn=collate_fn)
 
 
 def run(model, optim):
