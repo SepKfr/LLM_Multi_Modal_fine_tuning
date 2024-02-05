100af04ab0b4573581aa52e5175b86cefa1c71b8
sepidekoohfar <sepideh.koohfar@unh.edu>
2024-02-05 08:45:52 -0500

    Initial commit

---
+++ coarse_fine_grained.py
@@ -0,0 +1,180 @@
+import torch
+import torch.nn as nn
+from torch.nn import Transformer
+from DeepGP import DeepGPp
+
+
+class BlurDenoiseModel(nn.Module):
+
+    def __init__(self, d_model, num_inducing, gp, no_noise=False, iso=False):
+        """
+        Blur and Denoise model.
+        Args:
+        - model (nn.Module): Underlying forecasting model for adding and removing noise.
+        - d_model (int): Dimensionality of the model.
+        - num_inducing (int): Number of inducing points for the GP model.
+        - gp (bool): Flag indicating whether to use GP as the blur model.
+        - no_noise (bool): Flag indicating whether to add no noise during
+          denoising (denoise predictions directly).
+        - iso (bool): Flag indicating whether to use isotropic noise.
+        """
+        super(BlurDenoiseModel, self).__init__()
+
+        self.denoising_model = Transformer(d_model=d_model, dim_feedforward=d_model*4)
+
+        # Initialize DeepGP model for GP regression
+        self.deep_gp = DeepGPp(d_model, num_inducing)
+        self.gp = gp
+        self.sigma = nn.Parameter(torch.randn(1))
+
+        self.d = d_model
+        self.no_noise = no_noise
+        self.iso = iso
+
+    def add_gp_noise(self, x):
+        """
+        Add GP noise to the input using the DeepGP model.
+
+        Args:
+        - x (Tensor): Input tensor.
+
+        Returns:
+        - x_noisy (Tensor): Noisy input with added GP noise.
+        - dist (Tensor): GP distribution if GP is used.
+        """
+        b, s, _ = x.shape
+
+        # Predict GP noise and apply layer normalization
+        eps_gp = self.deep_gp.predict(x)
+
+        return eps_gp
+
+    def forward(self, inputs):
+        """
+        Forward pass of the BlurDenoiseModel.
+
+        Args:
+        - inputs (Tensor): Inputs.
+
+        Returns:
+        - output (Tensor): Denoised decoder output.
+        """
+        eps_enc = torch.randn_like(inputs)
+
+        if self.gp:
+            # Add GP noise to encoder and decoder inputs
+            input_noisy = self.add_gp_noise(inputs)
+
+        elif self.iso:
+            # Add scaled isotropic noise with trainable scale
+            input_noisy = inputs.add_(eps_enc * torch.clip(self.sigma, 0, 0.1))
+
+        else:
+            # No noise addition
+            input_noisy = inputs
+
+        # Perform denoising with the underlying forecasting model
+        outputs = self.denoising_model(input_noisy)
+
+        return outputs
+
+
+class PredictBlurDenoise(nn.Module):
+
+    def __init__(self, *,
+                 gp: bool = True,
+                 iso: bool = False,
+                 no_noise: bool = False,
+                 add_noise_only_at_training: bool = False,
+                 num_inducing: int,
+                 d_model: int = 32):
+        """
+        Forecast-blur-denoise Module.
+
+        Args:
+        - forecasting_model (nn.Module): The underlying forecasting model.
+        - gp (bool): Flag indicating whether to use GP as the blur model.
+        - iso (bool): Flag indicating whether to use Gaussian isotropic for the blur model.
+        - no_noise (bool): Flag indicating whether to add no noise during denoising
+         (denoise predictions directly).
+        - add_noise_only_at_training (bool): Flag indicating whether to add noise only during training.
+        - pred_len (int): Length of the prediction horizon.
+        - src_input_size (int): Number of features in input.
+        - tgt_output_size (int): Number of features in output.
+        - num_inducing (int): Number of inducing points for GP model.
+        - d_model (int): Dimensionality of the model (default is 32).
+        """
+        super(PredictBlurDenoise, self).__init__()
+
+        self.add_noise_only_at_training = add_noise_only_at_training
+        self.gp = gp
+        self.lam = nn.Parameter(torch.randn(1))
+
+        # Initialize the blur and denoise model
+        self.de_model = BlurDenoiseModel(d_model,
+                                         gp=gp,
+                                         no_noise=no_noise,
+                                         iso=iso,
+                                         num_inducing=num_inducing)
+
+        self.d_model = d_model
+
+    def forward(self, inputs):
+        """
+        Forward pass of the ForecastDenoising model.
+
+        Args:
+        - inputs (Tensor): Inputs.
+
+        Returns:
+        - outputs (Tensor): Model's predictions.
+        """
+
+        # Indicate whether to perform denoising
+        denoise = True
+
+        '''
+        If add_noise_only_at_training flag is set and during test 
+        do not perform denoising
+        '''
+
+        if self.add_noise_only_at_training and not self.training:
+            denoise = False
+
+        # Get outputs from the forecasting model
+        outputs = self.predictive_model(inputs)
+
+        if denoise:
+            # Apply denoising
+            model_outputs = self.de_model(outputs)
+        else:
+            model_outputs = outputs
+
+        return model_outputs
+
+
+class GPT2Classifier2(nn.Module):
+
+    def __init__(self, gpt_model, d_model, num_classes):
+
+        super(GPT2Classifier2, self).__init__()
+
+        self.gpt_model = gpt_model
+
+        self.proj_down = nn.Linear(gpt_model.config.vocab_size, d_model)
+
+        self.predict_blur_denoise = PredictBlurDenoise(num_inducing=32, d_model=d_model)
+
+        self.classification_head = nn.Linear(d_model, num_classes)
+
+    def forward(self, input_ids, attention_mask=None):
+
+        with torch.no_grad():
+
+            outputs = self.gpt_model(input_ids, attention_mask=attention_mask)
+
+        logits = self.proj_down(outputs.logits)
+        outputs = self.predict_blur_denoise(logits)
+        final_output = self.classification_head(outputs)
+
+        return final_output
---
+++ textgeneration.py
@@ -0,0 +1,122 @@
+import os
+
+from transformers import GPT2Tokenizer, GPT2LMHeadModel
+from datasets import load_dataset
+from torch.utils.data import DataLoader
+import torch
+import huggingface_hub
+
+from coarse_fine_grained import GPT2Classifier2
+from gptclassifier import GPT2Classifier
+
+huggingface_hub.login()
+
+# Set device for GPU usage (if available)
+device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
+
+train_data = load_dataset("yelp_review_full", split="train")
+train_eval = train_data.train_test_split(test_size=0.1, stratify_by_column="label")
+train_data = train_eval.get("train")
+valid_data = train_eval.get("test")
+test_data = load_dataset("yelp_review_full", split="test")
+
+product_reviews_train = train_data.filter(lambda x: "laptop" in x["text"])
+product_reviews_valid = valid_data.filter(lambda x: "laptop" in x["text"])
+product_reviews_test = test_data.filter(lambda x: "laptop" in x["text"])
+
+max_value = max(product_reviews_train, key=lambda x: x["label"])["label"]
+
+tokenizer = GPT2Tokenizer.from_pretrained("gpt2")
+tokenizer.pad_token = tokenizer.eos_token
+
+
+def collate_fn(batch):
+    # Extract sequences
+    sequences = [item["text"] for item in batch]
+
+    # Pad sequences using tokenizer directly
+    encoded_data = tokenizer(
+      sequences, padding=True, truncation=True, max_length=64
+    )
+
+    # Access padded input_ids and labels
+    padded_sequences = encoded_data["input_ids"]
+
+    padded_sequences = torch.tensor(padded_sequences, device=device)
+
+    # Filter out None values from labels:
+
+    labels = [item.get("label") for item in batch]
+    labels = torch.tensor(labels, device=device)
+
+    return padded_sequences, labels
+
+
+loss_fn = torch.nn.CrossEntropyLoss()
+
+train_dataloader = DataLoader(product_reviews_train, batch_size=4, collate_fn=collate_fn)
+valid_dataloader = DataLoader(product_reviews_valid, batch_size=4, collate_fn=collate_fn)
+test_dataloader = DataLoader(product_reviews_test, batch_size=4, collate_fn=collate_fn)
+
+
+def run(model, optim):
+    epochs = 3
+    for epoch in range(epochs):
+        model.train()
+        for i, batch in enumerate(train_dataloader):
+            input_ids, labels = batch
+            outputs = model(input_ids)
+            labels = torch.eye(max_value+1)[labels].to(torch.long)
+            loss = loss_fn(outputs, labels)
+            loss.backward()
+            optim.step()
+            optim.zero_grad()
+
+        print("train loss {:.3f}".format(loss.item()))
+
+        model.eval()
+        for batch in valid_dataloader:
+            input_ids, labels = batch
+            outputs = model(input_ids)
+            labels = torch.eye(max_value + 1)[labels].to(torch.long)
+            loss = loss_fn(outputs, labels)
+
+        print("valid loss {:.3f}".format(loss.item()))
+
+    model.eval()
+
+    tot_loss = 0
+    for batch in test_dataloader:
+        input_ids, labels = batch
+        outputs = model(input_ids)
+        labels = torch.eye(max_value + 1)[labels].to(torch.long)
+        loss = loss_fn(outputs, labels)
+        tot_loss += loss.item
+
+    print("test loss {:.3f}".format(loss))
+
+    return tot_loss
+
+
+gpt_model = GPT2LMHeadModel.from_pretrained("gpt2")
+
+gptclassifier = GPT2Classifier(gpt_model, max_value+1).to(device)
+optimizer = torch.optim.AdamW(gptclassifier.parameters(), lr=1e-5)
+
+test_loss1 = run(gptclassifier, optimizer)
+
+# gptclassifier2 = GPT2Classifier2(d_model=64, gpt_model=gpt_model, num_classes=max_value+1)
+# optimizer = torch.optim.AdamW(gptclassifier2.parameters(), lr=1e-5)
+#
+# test_loss2 = run(gptclassifier2, optimizer)
+
+print("loss of GPT: {:.3f}".format(test_loss1))
+#print("loss of GPT BlurDenoise: {:.3f}".format(test_loss2))
+
+# prompt = "Write a creative description for a product:"
+# new_description = model.generate(
+#     tokenizer.encode(prompt, return_tensors="pt").to(device),
+#     max_length=100, num_beams=2, early_stopping=True
+# )
+# decoded_description = tokenizer.decode(new_description[0], skip_special_tokens=True)
+# print(decoded_description)
