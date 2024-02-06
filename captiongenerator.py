6b7297d7784be7a540f878033ddb9016ea839fec
sepidekoohfar <sepideh.koohfar@unh.edu>
2024-02-06 15:48:11 -0500

    adding image caption again with a different dataset

--- captiongenerator.py
+++ captiongenerator.py
@@ -42,9 +42,8 @@ for inp in train_dataloader:
     output = model(**inp)
     labels = inp.pop("labels")
     logits = output["logits"]
+    print(logits.shape)
     log_probs = -nn.functional.log_softmax(logits, dim=-1)
-    print(log_probs.shape)
-    print(labels.shape)
     loss = log_probs.gather(dim=-1, index=labels)
     loss.backward()
     optimizer.step()
