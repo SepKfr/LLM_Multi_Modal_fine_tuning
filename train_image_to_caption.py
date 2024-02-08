5d99d4aece9dfaef139ddd136bd613333d27f40e
sepidekoohfar <sepideh.koohfar@unh.edu>
2024-02-07 20:28:23 -0500

    text classification for imdb

--- train_image_to_caption.py
+++ train_image_to_caption.py
@@ -1,15 +1,11 @@
-import numpy as np
 from datasets import load_dataset
 from torch import nn
-from torch.nn.utils.rnn import pad_sequence
 from torch.utils.data import DataLoader
 from transformers import AutoProcessor, AutoTokenizer, Adafactor
 from evaluate import load
 import torch
-from transformers import GitVisionModel, AutoModel
+from transformers import AutoModel
 from transformers.optimization import AdafactorSchedule
-
-CUDA_LAUNCH_BLOCKING=1
 
 
 class GitVisionModelClassifier(nn.Module):
@@ -127,92 +123,3 @@ def compute_metrics(eval_pred):
     decoded_predictions = processor.batch_decode(predicted, skip_special_tokens=True)
     wer_score = wer.compute(predictions=decoded_predictions, references=decoded_labels)
     return {"wer_score": wer_score}
-#
-#
-# from PIL import Image
-# import requests
-#
-# url = "https://huggingface.co/datasets/sayakpaul/sample-datasets/resolve/main/pokemon.png"
-# image = Image.open(requests.get(url, stream=True).raw)
-#
-# device = "cuda" if torch.cuda.is_available() else "cpu"
-#
-# inputs = processor(images=image, return_tensors="pt").to(device)
-# pixel_values = inputs.pixel_values
-#
-# generated_ids = model.generate(pixel_values=pixel_values, max_length=50)
-# generated_caption = processor.batch_decode(generated_ids, skip_special_tokens=True)[0]
-# print("generated caption:", generated_caption)
-# import torch
-#
-#
-# # Initializing a GitVisionConfig with microsoft/git-base style configuration
-# configuration = GitVisionConfig()
-#
-# # Initializing a GitVisionModel (with random weights) from the microsoft/git-base style configuration
-# model = GitVisionModel(configuration)
-#
-# # Accessing the model configuration
-# configuration = model.config
-#
-# from PIL import Image
-# import requests
-# from transformers import AutoProcessor, GitVisionModel
-#
-# processor = AutoProcessor.from_pretrained("microsoft/git-base")
-# model = GitVisionModel.from_pretrained("microsoft/git-base")
-#
-# url = "http://images.cocodataset.org/val2017/000000039769.jpg"
-# image = Image.open(requests.get(url, stream=True).raw)
-#
-# inputs = processor(images=image, return_tensors="pt")
-#
-# outputs = model(**inputs)
-# last_hidden_state = outputs.last_hidden_state
-#
-# from transformers import GitConfig, GitModel
-#
-# # Initializing a GIT microsoft/git-base style configuration
-# configuration = GitConfig()
-#
-# # Initializing a model (with random weights) from the microsoft/git-base style configuration
-# model = GitModel(configuration)
-#
-# # Accessing the model configuration
-# configuration = model.config
-#
-# from transformers import AutoProcessor, AutoModel
-# import requests
-# from PIL import Image
-#
-# processor = AutoProcessor.from_pretrained("microsoft/git-base")
-# model = AutoModel.from_pretrained("microsoft/git-base")
-#
-# url = "http://images.cocodataset.org/val2017/000000039769.jpg"
-# image = Image.open(requests.get(url, stream=True).raw)
-#
-# text = "this is an image of two cats"
-#
-# inputs = processor(text, images=image, return_tensors="pt")
-#
-# outputs = model(**inputs)
-# last_hidden_state = outputs.last_hidden_state
-#
-# from transformers import AutoProcessor, AutoModelForCausalLM
-# import requests
-# from PIL import Image
-#
-# processor = AutoProcessor.from_pretrained("microsoft/git-base-coco")
-# model = AutoModelForCausalLM.from_pretrained("microsoft/git-base-coco")
-#
-# url = "http://images.cocodataset.org/val2017/000000039769.jpg"
-# image = Image.open(requests.get(url, stream=True).raw)
-#
-# pixel_values = processor(images=image, return_tensors="pt").pixel_values
-#
-# generated_ids = model.generate(pixel_values=pixel_values, max_length=50)
-# generated_caption = processor.batch_decode(generated_ids, skip_special_tokens=True)[0]
-# print(generated_caption)
-
-
-
