4651d7043a7dec1d1fd626708f05801882d0b1a0
sepidekoohfar <sepideh.koohfar@unh.edu>
2024-02-07 20:16:28 -0500

    text classification for imdb

--- textgeneration.py
+++ textgeneration.py
@@ -39,16 +39,19 @@ def collate_fn(batch):
     return encoded_data.to(device), labels
 
 
-train_dataloader = DataLoader(imdb["train"], batch_size=64, collate_fn=collate_fn)
+train_eval = imdb["train"].train_test_split(test_size=0.1)
+train_dataloader = DataLoader(train_eval["train"], batch_size=64, collate_fn=collate_fn)
+val_dataloader = DataLoader(train_eval["test"], batch_size=64, collate_fn=collate_fn)
 test_dataloader = DataLoader(imdb["test"], batch_size=64, collate_fn=collate_fn)
 
 loss_fn = nn.CrossEntropyLoss()
 epochs = 50
-
+best_eval_loss = 1e10
+check_p_epoch = 0
 for epoch in range(epochs):
     tot_loss = 0
     model.train()
-    for i, batch in enumerate(train_dataloader):
+    for batch in train_dataloader:
 
         inputs, labels = batch
         outputs = model(**inputs)
@@ -60,7 +63,23 @@ for epoch in range(epochs):
         lr_scheduler.step()
         optimizer.zero_grad()
 
-    print("loss: {:.3f}".format(tot_loss))
+    model.eval()
+    eval_loss = 0
+    for batch in val_dataloader:
+        inputs, labels = batch
+        outputs = model(**inputs)
+        predicted = outputs.logits
+        loss = loss_fn(predicted, labels)
+        eval_loss += loss.item()
+
+    print("train loss: {:.3f}".format(tot_loss))
+    print("valid loss: {:.3f}".format(eval_loss))
+    if eval_loss < best_eval_loss:
+        best_eval_loss = eval_loss
+        check_p_epoch = epoch
+    if epoch - check_p_epoch >= 5:
+        break
+
 
 model.eval()
 tot_acc = 0
@@ -69,7 +88,7 @@ for batch in test_dataloader:
     predicted = model(**inputs).logits
     predicted = torch.argmax(predicted, dim=-1)
     acc = accuracy.compute(predictions=predicted, references=labels)
-    tot_acc += acc
+    tot_acc += acc["score"]
 
 print("total accuracy: {:.3f}".format(tot_acc/len(test_dataloader)))
 
