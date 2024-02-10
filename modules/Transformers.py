5faa442c2a76ab84cabb25221dd7a82840325222
sepidekoohfar <sepideh.koohfar@unh.edu>
2024-02-10 12:18:07 -0500

    adding fine_tune models

--- modules/Transformers.py
+++ modules/Transformers.py
@@ -15,14 +15,12 @@ class PositionalEncoding(nn.Module):
 
         super(PositionalEncoding, self).__init__()
         # Create a long enough `P`
-        d_hid = d_hid * 2
-        self.P = torch.zeros((1, max_len, d_hid)).to(device)
+        self.P = torch.zeros((1, max_len*2, d_hid)).to(device)
         X = torch.arange(max_len, dtype=torch.float32).reshape(
             -1, 1) / torch.pow(10000, torch.arange(
             0, d_hid, 2, dtype=torch.float32) / d_hid)
         self.P[:, :, 0::2] = torch.sin(X)
         self.P[:, :, 1::2] = torch.cos(X)
-        print(self.P.shape)
 
     def forward(self, X):
         X = X + self.P[:, :X.shape[1], :].to(X.device)
